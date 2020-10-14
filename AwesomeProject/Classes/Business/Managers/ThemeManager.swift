89fc47b5ed1070bf4858b82a876691f3333bbf8c
aps21 <ap.nnn1@gmail.com>
2020-10-15 01:55:27 +0300

    Some code improvements

--- AwesomeProject/Classes/Business/Managers/ThemeManager.swift
+++ AwesomeProject/Classes/Business/Managers/ThemeManager.swift
@@ -9,22 +9,36 @@ class ThemeManager: ThemesPickerDelegate {
         static let themeKey = "ThemeManager.themeKey"
     }
 
+    private var currentTheme: Theme = .classic
+
+    private let queue = DispatchQueue.global(qos: .userInitiated)
     private let userDefaults: UserDefaults
     private let notificationCenter: NotificationCenter
 
-    private(set) var theme: Theme
+    var theme: Theme {
+        get {
+            var tempTheme: Theme = .classic
+            queue.sync { tempTheme = self.currentTheme }
+            return tempTheme
+        }
+        set {
+            queue.async {
+                self.currentTheme = newValue
+                self.userDefaults.set(newValue.rawValue, forKey: Constants.themeKey)
+                self.notificationCenter.post(name: .themeUpdate, object: newValue)
+            }
+        }
+    }
 
     static let shared = ThemeManager()
 
     private init(userDefaults: UserDefaults = .standard, notificationCenter: NotificationCenter = .default) {
         self.userDefaults = userDefaults
         self.notificationCenter = notificationCenter
-        theme = Theme(rawValue: userDefaults.integer(forKey: Constants.themeKey)) ?? .classic
+        currentTheme = Theme(rawValue: userDefaults.integer(forKey: Constants.themeKey)) ?? .classic
     }
-
+    
     func didSelect(theme: Theme) {
         self.theme = theme
-        userDefaults.set(theme.rawValue, forKey: Constants.themeKey)
-        notificationCenter.post(name: .themeUpdate, object: theme)
     }
 }
